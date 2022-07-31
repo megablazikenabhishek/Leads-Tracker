5055a05d760ef1920c875084c3441e847c995624
Abhishek <megablazikenabhishek@gmail.com>
2022-07-31 13:29:32 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,70 @@
+let myLeads = [];
+
+const saveBtn = document.getElementById("save-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+const deleteEl = document.getElementById("delete-btn");
+const saveTabs = document.getElementById("tab-btn");
+
+//checking if there is any Local Storage available
+let checkforLeads = JSON.parse(localStorage.getItem("myLeads"));
+if(checkforLeads) {
+    myLeads = checkforLeads;
+    // console.log(myLeads);
+    renderLeads();
+}
+
+
+saveBtn.addEventListener("click", function(){
+    save();
+});
+
+inputEl.addEventListener("keypress", function(event){
+    if(event.key == "Enter") {
+        save();
+    }
+})
+
+deleteEl.addEventListener("dblclick", function() {
+    console.log(localStorage.clear());
+    myLeads = [];
+    ulEl.innerHTML = ""
+})
+
+saveTabs.addEventListener("click", function(){
+    //using chrome API to capture the url
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) );
+        renderLeads();
+    })
+})
+
+function save()
+{
+    myLeads.push(inputEl.value);
+
+        ulEl.innerHTML = "";
+        inputEl.value = "";
+
+        //saving into local storage
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        console.log(localStorage.getItem("myLeads"));
+        
+        //rendering the Leads
+        renderLeads();
+}
+
+function renderLeads()
+{
+    for(let i=0; i<myLeads.length; i++)
+        {
+            ulEl.innerHTML += `
+                <li>
+                    <a target = '_blank' href = '${myLeads[i]}' >
+                    ${myLeads[i]}
+                    </a>
+                </li>
+            `
+        }
+}
